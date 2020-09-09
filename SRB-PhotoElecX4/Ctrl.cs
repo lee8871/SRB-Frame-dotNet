59a808a12fedd76849e9245fb7a50f69dbf5029b
lee8871 <lee8871@126.com>
2020-09-09 18:43:11 +0800

    更新了4光电程序（未完成）

--- SRB-PhotoElecX4/Ctrl.cs
+++ SRB-PhotoElecX4/Ctrl.cs
@@ -16,9 +16,23 @@ namespace SRB.NodeType.PhotoElecX4
         {
             datas = (Interpreter)n.Datas;
             InitializeComponent();
-            n.eBankChangeByAccess += N_eBankChangeByAccess;
             node = n;
+            node.eBankChangeByAccess += N_eBankChangeByAccess;
             tableInit();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            node.eBankChangeByAccess -= N_eBankChangeByAccess;
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
         private void tableInit()
@@ -26,13 +40,10 @@ namespace SRB.NodeType.PhotoElecX4
             for (int i = 0; i < datas.Sensor_num; i++) {
                 
                int col=ADCtable.Columns.Add("S" + i, "S" + i);
-
                 ADCtable.Columns[col].Width = 40;
                 ADCtable.Columns[col].ReadOnly = true;
-
             }
         }
-
         private void N_eBankChangeByAccess(object sender, EventArgs e)
         {
             for (int i = 0; i<datas.Sensor_num; i++)
@@ -41,10 +52,8 @@ namespace SRB.NodeType.PhotoElecX4
                 {
                     ADCtable[i, 0].Value = datas.value(i);
                 }
-
             }
         }
-
         protected override void OnAccess()
         {
             base.OnAccess();
--- SRB-PhotoElecX4/PhotoElecX4.cs
+++ SRB-PhotoElecX4/PhotoElecX4.cs
@@ -3,33 +3,59 @@ using System;
 
 namespace SRB.NodeType.PhotoElecX4
 {
+    public class sGroupPheValue
+    {
+        protected virtual int size => 8;
+        protected byte[] ba;
+        protected int diff;
+
+        public void init(byte[] ba, int diff)
+        {
+            this.ba = ba;
+            this.diff = diff;
+        }
+        public void fork()
+        {
+            byte[] my_ba = new byte[size];
+            for(int i=0;i< size; i++)
+            {
+                my_ba[i] =  ba[diff + i];
+            }
+            ba = my_ba;
+            diff = 0;
+        }
+
+
+
+    } 
+
+
     public class Interpreter : Node.INodeInterpreter
     {
         public int Sensor_num { get => 4; }
         internal MappingCluster Mapping0_clu;
         public override string Help_net_work =>
             "https://github.com/lee8871/SRB-Introduction/blob/master/SRB%E5%8F%8C%E7%94%B5%E6%9C%BA%E8%8A%82%E7%82%B9.md";
+        public int value(int num) => (short)bank.getBankUshort(num * 2);
         
-        public int value(int num) => (short)bank.getBankUshort(num*2);
 
 
-
+        public byte gpv_in_bank => bank.getBankByte(3);
         public void init()
         {
             Mapping0_clu = new MappingCluster(3, Node, "Mapping0");
 
             Mapping0_clu.eDataChanged += updataMapping;
             Mapping0_clu.read();
-
         }
-
+        
         private void updataMapping(object sender, EventArgs e)
         {
             Node.bankInit(new byte[][]{
                 Mapping0_clu.mapping                  ,
-                new byte[] {9,0,    0,1,2,3,4,5,6,7, 8}              ,
-                new byte[] {8,0,    0,1,2,3,4,5,6,7}             ,
-                new byte[] {8,0,    0,1,2,3,4,5,6,7}
+                new byte[] {9,1,	3,4,5,6,7,8,9,10,11,2,},
+                new byte[] {9,3,	3,4,5,6,7,8,9,10,11,0,1,2,},
+                new byte[] {9,1,    3,4,5,6,7,8,9,10,11,2,},
             });
         }
         public Interpreter(Node n)
@@ -45,5 +71,7 @@ namespace SRB.NodeType.PhotoElecX4
             return new Ctrl(Node);
         }
         public override string Describe => @"This node drivers two motors. Without speed or force sensor";
+
+
     }
 }
